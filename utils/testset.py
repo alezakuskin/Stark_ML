505789ed2d288a23d9876bb933716955037c809e
alezakuskin <ale-zakuskin@yandex.ru>
2022-04-11 13:05:56 +0300

    Create testset.py

---
+++ utils/testset.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def test_selection(data, target, p = 0.2, low_limit = 'min', up_limit = 'max', random_state = None):
+  if low_limit != 'min':
+    low_limit = str(low_limit) + '%'
+  if up_limit != 'max':
+    up_limit = str(up_limit) + '%'
+  
+  stats = target.describe()
+  target_range = target[(target >= stats[low_limit]) & (target <= stats[up_limit])]
+  target_selected = target_range.sample(frac = p, random_state = random_state)
+  data_selected = data.loc[target_selected.index.to_list()]
+
+  return data_selected, target_selected
+
