f644f89868939b937793a229dd9ac6b6d8ee8b9b
alezakuskin <ale-zakuskin@yandex.ru>
2022-04-11 19:14:42 +0300

    Revert "Update optuning.py"
    This reverts commit df98f638e6badbbf25cdffb87cd074175c9e88cc.

--- utils/optuning.py
+++ utils/optuning.py
@@ -46,7 +46,7 @@ def main(X, y, model_name, params, n_trials = 100):
     
     Sampler = optuna.samplers.TPESampler(seed = 777)
     study = optuna.create_study(sampler = Sampler)
-    study.optimize(Objective(model_name, X, y, params), n_trials, show_progress_bar = True)
+    study.optimize(Objective(model_name, X, y, params), n_trials, show_progress_bar = True, n_jobs = -1)
     print("Best parameters:", study.best_trial.params)
 
     return study
