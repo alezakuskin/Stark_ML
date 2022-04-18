db92498db885a65b9589e6c73a9a7816cb95ef3a
alezakuskin <ale-zakuskin@yandex.ru>
2022-04-18 19:40:53 +0300

    Update tabnet.py

--- models/tabnet.py
+++ models/tabnet.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import TensorDataset, DataLoader
 
-from Stark_ML.models.basemodel import BaseModel
+from Stark_ML.models.base_models import BaseModel
 
 
 class BaseModelTorch(BaseModel):
