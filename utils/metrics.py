e9f897547fdc434ee98beea2a8c0cb4603b29ed8
alezakuskin <ale-zakuskin@yandex.ru>
2022-04-21 17:58:02 +0300

    Update metrics.py

--- utils/metrics.py
+++ utils/metrics.py
@@ -35,6 +35,7 @@ def train_and_test_regressor(models, X_train, y_train, X_test, y_test, max_epoch
   X_test_save, y_test_save = X_test, y_test
   predictions = {}
   for name, model in models.items():
+  print(type(model))
     if isinstance(model, TabNetRegressor):
       X_train, X_test = X_train_save.to_numpy(), X_test_save.to_numpy()
       y_train, y_test = y_train_save.to_numpy().reshape(-1, 1), y_test_save.to_numpy().reshape(-1, 1)
