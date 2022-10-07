ef13f4a34f18b59f9ea224f042541c461da2ee69
alezakuskin <ale-zakuskin@yandex.ru>
2022-10-07 19:46:40 +0300

    Update tabnet.py

--- models/tabnet.py
+++ models/tabnet.py
@@ -85,7 +85,7 @@ class TabNet(BaseModelTorch):
 
         self.params["device_name"] = self.device'''
 
-        self.model = TabNetRegressor(**params, n_a = params['n_d'], verbose = False, seed = np.random.randint(1000))
+        self.model = TabNetRegressor(**params, n_a = params['n_d'], verbose = False, seed = 0)
         
     def fit(self, X, y, X_val=None, y_val=None):
         X = X.to_numpy()
