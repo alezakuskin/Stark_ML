6908e9ccb68578b13dd65ed9f2125e153863bf89
alezakuskin <ale-zakuskin@yandex.ru>
2023-03-24 17:20:46 +0300

    Update base_models.py

--- models/base_models.py
+++ models/base_models.py
@@ -281,7 +281,7 @@ class LightGBM(BaseModel):
     def __init__(self, params):
         super().__init__(params)
         
-        self.model = lgb.LGBMRegressor(**params, n_jobs = -1, silent = True)
+        self.model = lgb.LGBMRegressor(**params, n_jobs = -1, verbose = 0)
     
     @classmethod
     def define_trial_parameters(cls, trial, params):
