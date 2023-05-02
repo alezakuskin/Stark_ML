52620837d848bfb986ea7636269b2678eb58b534
Aleksandr Zakuskin <ale-zakuskin@yandex.ru>
2023-05-02 17:40:19 +0300

    added gap_to_ion function

--- utils/terms.py
+++ utils/terms.py
@@ -1,3 +1,5 @@
+import Stark_ML
+
 def term_to_number(term):
   import pandas as pd
   momentum = pd.Series()
@@ -22,3 +24,13 @@ def term_to_number(term):
       raise NameError(f'Term symbol "{val}" is not specified')
   return momentum
 
+
+
+def gap_to_ion(data, column_name = None, file = Stark_ML.__path__.__dict__['_path'][0] + '/Source_files/E_ion.csv'):
+    ion_Es = pd.read_csv(file)
+    gap = pd.Series()
+    for index, val in enumerate(data['Gap to ion']):
+        gap.at[index] = float(ion_Es.loc[ion_Es['Element'] == data.loc[index]['Element']][str(data.loc[index]['Charge'])]) - data.loc[index][column_name]
+        if np.isnan(gap.at[index]):
+            print(f"Please find and insert to '/Source_files/E_ion.csv' ionization energy value for {data.loc[index]['Element']} with charge {data.loc[index]['Charge']}")
+    return gap
