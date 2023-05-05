2a3c16f31e20f8191a9d0bd4f4fa3d72ac02f0a6
Aleksandr Zakuskin <ale-zakuskin@yandex.ru>
2023-05-05 16:57:48 +0300

    CRITICAL update of train test split

--- utils/testset.py
+++ utils/testset.py
@@ -6,7 +6,10 @@ def test_selection(data, target, p = 0.2, low_limit = 'min', up_limit = 'max', r
     up_limit = str(up_limit) + '%'
   
   stats = target.describe()
-  target_range = target[(target >= stats[low_limit]) & (target <= stats[up_limit])]
+  target_range = target[(target >= stats[low_limit]) & (target < stats[up_limit])]
+  if up_limit == 'max':
+    target_range = target[(target >= stats[low_limit]) & (target <= stats[up_limit])]
+    
   target_selected = target_range.sample(frac = p, random_state = random_state)
   data_selected = data.loc[target_selected.index.to_list()]
 
