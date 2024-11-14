8c9021caa4cf4c837dffbddd0a393ce6144d7fdc
Aleksandr Zakuskin <ale-zakuskin@yandex.ru>
2024-11-14 16:21:36 +0300

    get functions moved here
    _get functions moved here from Stark.py

--- utils/comms.py
+++ utils/comms.py
@@ -1,6 +1,14 @@
 import re
+import json
 import roman
 import mariadb
+import Stark_ML
+import pandas as pd
+
+from Stark_ML.utils.encoding import *
+
+with open('Stark_ML/credentials.json', 'r') as file:
+    creds = json.load(file)
 
 
 def connect_to_DB(username,
@@ -76,3 +84,103 @@ def convert_species_request(s):
             raise ValueError("Invalid input format")
     
     return elements, ionizations
+    
+
+def get_lines_from_DB(elements: str, lower: str(float), upper: str(float), count_mode = False, save_for_manual_check = False):
+        if count_mode:
+            lines_count = _handle_query(elements, lower, upper, count_mode)
+            return lines_count
+        else:
+            DB_df = _handle_query(elements, lower, upper, count_mode)
+            data_i = pd.read_excel(Stark_ML.__path__.__dict__['_path'][0] + '/Source_files/Stark_data.xlsx',
+                                   sheet_name='Ions',
+                                   usecols='A:BQ',
+                                   nrows = 2
+                               )
+            try:
+                request_df, lines_for_check = split_OK_check(DB_to_StarkML(DB_df, data_i), save_manual_check = save_for_manual_check, save_txts = False)
+                return request_df, lines_for_check
+            except UserDefinedError as e:
+                raise UserDefinedError(e)
+                
+                
+def _handle_query(spectra: str,
+                    lower: float,
+                    upper: float,
+                    count_mode: bool):
+        elements, ionizations = convert_species_request(spectra)
+
+        connection = connect_to_DB(username = creds['username'],
+                                  password = creds['password'])
+        cur = connection.cursor()
+        
+        if count_mode:
+            response = _get_count_lines_from_DB(cur, elements, ionizations, lower, upper)
+        else:
+            response = _get_lines_from_DB(cur, elements, ionizations, lower, upper)
+        cur.close()
+        connection.close()
+        return response
+
+
+def _get_lines_from_DB(cursor, elements, ionizations, lower_wl, upper_wl):
+    DB_df = None
+    for i in range(len(elements)):
+        el = elements[i]
+        ion = ionizations[i]
+        if ion != 'All':
+            query = f'''
+            SELECT *
+            FROM mytestview2
+            WHERE airwl >= ?
+            AND airwl <= ?
+            AND el_name = ?
+            AND ion_stage in {f"({', '.join(map(str, ion))})"}
+            '''
+        else:
+            query = f'''
+            SELECT *
+            FROM mytestview2
+            WHERE airwl >= ?
+            AND airwl <= ?
+            AND el_name = ?
+            '''
+        cursor.execute(query, (lower_wl, upper_wl, el))
+        column_names = [desc[0] for desc in cursor.description]
+        req_results = cursor.fetchall()
+        req_results = pd.DataFrame(req_results, columns=column_names)
+        if not req_results.empty:
+            if DB_df is None:
+                DB_df = req_results
+            else:
+                DB_df = pd.concat([DB_df, req_results], ignore_index=True)
+    return DB_df
+        
+        
+def _get_count_lines_from_DB(cursor, elements, ionizations, lower_wl, upper_wl):
+    count = 0
+    for i in range(len(elements)):
+        el = elements[i]
+        ion = ionizations[i]
+        if ion != 'All':
+            query = f'''
+            SELECT COUNT(*)
+            FROM mytestview2
+            WHERE airwl >= ?
+            AND airwl <= ?
+            AND el_name = ?
+            AND ion_stage in {f"({', '.join(map(str, ion))})"}
+            '''
+        else:
+            query = f'''
+            SELECT COUNT(*)
+            FROM mytestview2
+            WHERE airwl >= ?
+            AND airwl <= ?
+            AND el_name = ?
+            '''
+        cursor.execute(query, (lower_wl, upper_wl, el))
+        req_results = cursor.fetchall()[0][0]
+        DB_df = req_results
+        count += req_results
+    return count
